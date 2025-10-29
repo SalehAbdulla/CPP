21d867e342db36496e86df3ed1f61245ded1ae9b
Saleh Abdulla <salehabdullaict@gmail.com>
2025-10-29 19:43:39 +0300

    Variables in C++

---
+++ HelloWorld.cpp/mainVariables.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nonem on 29/10/2025.
+//
+
+#include "mainVariables.h"
---
+++ HelloWorld.cpp/mainVariables.h
@@ -0,0 +1,14 @@
+//
+// Created by nonem on 29/10/2025.
+//
+
+#ifndef HELLOWORLD_CPP_MAINVARIABLES_H
+#define HELLOWORLD_CPP_MAINVARIABLES_H
+
+
+class mainVariables {
+
+};
+
+
+#endif //HELLOWORLD_CPP_MAINVARIABLES_H
