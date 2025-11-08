526155b201a4474d5ae61da5a700eac001476ad2
Saleh Abdulla <salehabdullaict@gmail.com>
2025-11-08 23:31:03 +0300

    First Sessions C++

---
+++ Section4/Section4mySolution/FirstProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int favoriteNumber;
+    std::cout << "Enter Your Favorite number between 1 and 100: ";
+    std::cin >> favoriteNumber;
+    std::cout << "Amazing!! That's my Favorite number \n";
+    return 0;
+}
